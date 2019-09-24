b2fa14571ef37aab7a8485156852ae3e9c49e15e
Artem Abramov <tanukisuicide@yandex.ru>
2019-09-24 22:13:44 +0300

    Add posts list

---
+++ components/Promo.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="promo">
+    <div class="container">
+      <h1 class="title">My SSP Blog! with Nuxt.js</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta at iste atque distinctio dignissimos ratione autem itaque qui, quia voluptas!</p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.promo {
+  text-align: center;
+
+  p {
+    color: #999;
+  }
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="about">
+      <div class="container">
+        <h1 class="title">About my blog: </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aliquam commodi ratione eaque minima, est alias voluptatem consectetur inventore ipsam!</p>
+        <intro title='Lorem ipsum dolor sit amet consectetur' />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aliquam commodi ratione eaque minima, est alias voluptatem consectetur inventore ipsam!</p>
+        <img src="@/assets/img/about.jpg" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia aliquam commodi ratione eaque minima, est alias voluptatem consectetur inventore ipsam!</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import intro from '@/components/UI/Intro.vue'
+export default {
+  components: { intro }
+}
+</script>
+
+<style lang="scss">
+.about {
+  text-align: center;
+
+  p {
+    margin-bottom: 10px;
+  }
+}
+</style>
